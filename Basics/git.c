ff84d02390c663e5c649cb418d09a9507e592849
avnish-prajapati <avnishprajapati664@gmail.com>
2025-05-05 17:54:18 +0530

    Create git.c

---
+++ Basics/git.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	
+//	int x = 10,y=20,z;
+//	
+//	z = x; // z=10
+//	x = y; // x=20
+//	y = z; // y=10
+//	
+//	printf("x = %d\n",x);
+//	printf("y = %d",y);
+//	
+	
+	// p1 - p4
+	// GITHUB - developers - server - codebase
+	// version control system
+	// used to push and pull code and use everywhere.
+	
+	// snapchat - streak - 600
+	// @vnish2002
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
