9e47bfbe6b753fc5869f3ae522e8ed76ee9798fa
Avnish Prajapati <avnishprajapati664@gmail.com>
2025-06-16 15:14:32 +0530

    Update Q7.c

--- Submission/cicrulat/Q7.c
+++ Submission/cicrulat/Q7.c
@@ -1,24 +1,21 @@
-#include<stdio.h>
-#include<conio.h>
+#include<stdio.h> //standard input output headfile
+#include<conio.h>	//console input output headfile..
+
 // * * * * *
 // *       *
 // * * * * *
 // *
 // *
-void main ()
+
+void main () // its starting line if code..
 {
+   int i,j; // A datatype type <int> which contains two varibale i,j,
+   
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			printf("");
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
