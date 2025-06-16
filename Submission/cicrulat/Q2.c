456e4fdfccfba1b323a7a2e7a9295966a8c25bc8
Avnish Prajapati <avnishprajapati664@gmail.com>
2025-06-16 15:09:25 +0530

    Update Q2.c

--- Submission/cicrulat/Q2.c
+++ Submission/cicrulat/Q2.c
@@ -8,11 +8,11 @@
 
 void main()
 {
-    int i, j, k=11;
+    int i,j,k=11;
 
-    for (i=1; i<=4; i++)
+    for(i=1; i<=4; i++)
     {
-        for (j=1; j<=i; j++)
+        for(j=1; j<=i; j++)
         {
             printf("%d ", k);
             k++;
