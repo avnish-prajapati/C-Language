c78752a9e54b2346a14f19433dbd05b26018eec0
Avnish Prajapati <avnishprajapati664@gmail.com>
2025-06-16 14:41:08 +0530

    Update Q4.c

--- Submission/cicrulat/Q4.c
+++ Submission/cicrulat/Q4.c
@@ -1,11 +1,39 @@
-#include<stdio.h>
-#include<conio.h>
+#include<stdio.h> //standard input output headfile..
+#include<conio.h>	//console input output headfile..
 
 // 1 0 1 0 1
 //   1 0 1 0
 //     1 0 1  
 //       1 0
 //         1
-void main()
+
+void main()  // its starting point of codes..
 {
-    int i,j;
+	int i,j;  // A datatype type <int> which contains two varibale i , j .. 
+
+    for(i=5; i>=1; i--)
+    {
+    	int k; // another variable in need to used space between value..
+    	
+        for(k=5; k>i; k--)
+            {
+                printf("  ");
+            }
+            
+            for(j=1; j<=i; j++)  
+            {
+        
+                if(j%2==0)  // even or odd formate used to select 0 or 1 output for patterns..
+                {
+                    printf("0 ");
+                }
+                else 
+                {
+                    printf("1 ");
+                }
+                
+            }
+            printf("\n"); // for line break...
+        }
+        
+}
