9e0d256ba6001a9a01a0f24dca462877aca47110
Avnish Prajapati <avnishprajapati664@gmail.com>
2025-07-19 17:44:58 +0530

    Create Matrix.c

---
+++ Submission/PR.8/Matrix.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void cube(int *p, int rows, int cols)
+{
+    for (int i = 0; i < rows * cols; i++)
+    {
+        *(p + i) = (*(p + i)) * (*(p + i)) * (*(p + i));
+    }
+}
+
+void matrix(int *p, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d\t", *(p + i * cols + j));
+        }
+        printf("\n");
+    }
+}
+
+void main()
+{
+    int r, c;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    printf("\nOriginal Matrix:\n");
+    matrix(&a[0][0], r, c);
+
+    cube(&a[0][0], r, c);
+
+    printf("\nMatrix after cubing each element:\n");
+    matrix(&a[0][0], r, c);
+}
