be149374d33074b3e0956f09748f9577446020d6
Avnish Prajapati <avnishprajapati664@gmail.com>
2025-07-19 11:21:48 +0530

    Update calicus.c

--- Submission/PR.7/calicus.c
+++ Submission/PR.7/calicus.c
@@ -8,58 +8,62 @@ void calicus(int a, int b)
   do
   {
     printf("\nOptions:\n");
+
     printf("1.Add\n");
     printf("2.Sub\n");
+
     printf("3.Multiply\n");
     printf("4.Divide\n");
+
     printf("5.Modulus (%%)\n");
     printf("6.Exit\n");
 
     printf("Enter your choice: ");
-    scanf("%d", &choose);
+    scanf("%d",&choose);
 
     switch (choose)
     {
     case 1:
-      printf("%d + %d = %d\n", a, b, a + b);
+      printf("%d + %d = %d\n",a, 
+b,a + b);
       break;
     case 2:
-      printf("%d - %d = %d\n", a, b, a - b);
+      printf("%d - %d = %d\n",a,b,a - b);
       break;
     case 3:
-      printf("%d * %d = %d\n", a, b, a * b);
+      printf("%d * %d = %d\n",a,b, a * b);
       break;
     case 4:
       if (b != 0)
-        printf("%d / %d = %d\n", a, b, a / b);
+        printf("%d / %d = %d\n",a,b,a / b);
       else
-        printf("Divide by 0 not allowed!\n");
+        printf("Divide by 0 not allowed\n");
       break;
     case 5:
       if (b != 0)
-        printf("%d %% %d = %d\n", a, b, a % b);
+        printf("%d %% %d = %d\n",a,b,a % b);
       else
-        printf("Modulus by 0 not allowed!\n");
+        printf("Modulus by 0 not allowed\n");
       break;
     case 6:
       printf("Exiting...\n");
       break;
     default:
-      printf("Invalid choice! Try again.\n");
+      printf("Invalid choice.. Try again.\n");
       break;
     }
   } while (choice != 6);
 }
 
-void main()
+int main()
 {
   int a, b;
 
   printf("Enter a num: ");
-  scanf("%d", &a);
+  scanf("%d",&a);
 
   printf("Enter b num: ");
-  scanf("%d", &b);
+  scanf("%d",&b);
 
   calicus(a,b); 
 }
