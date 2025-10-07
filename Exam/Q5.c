7969992af0b18085d2026e6e016944aee06fe8b3
Avnish Prajapati <avnishprajapati664@gmail.com>
2025-10-07 16:30:03 +0530

    Update Q5.c

--- Exam/Q5.c
+++ Exam/Q5.c
@@ -1,41 +1,24 @@
-#include<stdio.h>
-#include<conio.h>
-//Print a below pattern using nested for loop in C language:
-//100 
-// -19
-//81 81
-// -17
-//64 64 64
-// -15
-//49 49 49 49
-// -13
-//36 36 36 36 36
-void main ()
-// even number 19,17,15,13....
-{
-	int i,j;
-	
-	for(i=1; i<=100; i++) // outer loop 
-	{
-		for(j=1; j<i; j++)  // inner loop 
-		{
-			printf("%d ",i);
-			
-		if(i%=0)
-		{
-			printf("%d ",i);
-		}
-		else{
-			
-			}	
-		}
-		printf("\n");
-	}
-	
-	
-	
-	
-	
-	
-	
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int evenNum = 10;   // Starting number for square values
+    int negOdd = -19;   // Starting negative odd number
+
+    for (int i = 0; i < 9; i++) {  // 9 lines total
+        if (i % 2 == 0) {
+            // Print square of evenNum, (i/2 + 1) times
+            int square = evenNum * evenNum;
+            for (int j = 0; j <= i / 2; j++) {
+                printf("%d ", square);
+            }
+            evenNum--;  // Decrease for next line
+        } else {
+            printf("%d", negOdd);
+            negOdd += 2;  // Next negative odd number
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
